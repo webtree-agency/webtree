7b877562e6f0d53f9b7c23dd7b964aba54b1cd85
Noah Benz <noah.benz@edu.tbz.ch>
2023-09-22 13:16:28 +0200

    feat: Mouse follower

--- content/assets/js/script.js
+++ content/assets/js/script.js
@@ -18,26 +18,16 @@ setTimeout(function () {
 
 
 
-const coords = {x: 0,y: 0};
+const coords = {x: 0, y: 0};
 const circles = document.querySelectorAll(".circle");
 
-circles.forEach(function (circle){
-circle.x = 0;
-circle.y = 0;
-});
-
-
-window.addEventListener("mousemove", function(e){
+window.addEventListener("mousemove", function(e) {
     coords.x = e.clientX;
-    coords.y = e.clientY
+    coords.y = e.clientY;
 
-    circles.forEach(function(circle, index){
-        circle.style.left = coords.x - 15 + "px";
-        circle.style.top = coords.y - 15 + "px";
-    
-        x += 10;
-        y += 10;
-
-
+    circles.forEach(function(circle, index) {
+        circle.style.left = coords.x + "px";
+        circle.style.top = coords.y + "px";
     });
 });
+
