0349bc90951cdc53013688a0380e6ee4b6ec961e
jannfanzun <jannandrea.f@gmail.com>
2023-09-22 15:15:21 +0200

    feat: add arrow up while scrolling

--- content/assets/js/script.js
+++ content/assets/js/script.js
@@ -15,3 +15,24 @@ function typeWriter(text, index) {
 setTimeout(function () {
     typeWriter(text, 0); // Animation starten nach kurzem Timeout
 }, 2800); // Kurzer Timeout vor Beginn der Animation (in Millisekunden)
+
+let mybutton = document.querySelector(".svgContainer");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
