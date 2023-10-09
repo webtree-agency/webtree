e270741a4d688b469a4f1033bfd220b8e06a6b16
jannfanzun <jannandrea.f@gmail.com>
2023-10-09 16:31:47 +0200

    wip: impressum and arrow on bottom

--- content/assets/js/script.js
+++ content/assets/js/script.js
@@ -71,6 +71,27 @@ function topFunction() {
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
+let mybutton_impressum = document.querySelector(".svgContainer-impressum");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        mybutton_impressum.style.display = "block";
+    } else {
+        mybutton_impressum.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 // Eine Funktion, die überprüft, ob ein Element im Viewport sichtbar ist
 function isElementInViewport(el) {
     var rect = el.getBoundingClientRect();
