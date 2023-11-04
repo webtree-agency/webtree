231eb926286b281fe48e48f9e26b3c56a5d76680
jannfanzun <110892321+jannfanzun@users.noreply.github.com>
2023-11-04 22:26:37 +0100

    fix: switch on services

--- content/assets/js/script.js
+++ content/assets/js/script.js
@@ -384,6 +384,24 @@ $('.scroll-to-section a[href*="#"]').on('click', function() {
   }
 });
 
+
+    // Acc
+    $(document).on("click", ".naccs .menu div", function() {
+      var numberIndex = $(this).index();
+
+      if (!$(this).is("active")) {
+          $(".naccs .menu div").removeClass("active");
+          $(".naccs ul li").removeClass("active");
+
+          $(this).addClass("active");
+          $(".naccs ul").find("li:eq(" + numberIndex + ")").addClass("active");
+
+          var listItemHeight = $(".naccs ul")
+            .find("li:eq(" + numberIndex + ")")
+            .innerHeight();
+          $(".naccs ul").height(listItemHeight + "px");
+        }
+    });
       // Page loading animation
        $(window).on('load', function() {
   
