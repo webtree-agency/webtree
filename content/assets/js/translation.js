80fef5fe23b4bf2cc7018f5eb9a9e77f5e6a0222
Noah Benz <noah.benz@edu.tbz.ch>
2024-03-25 21:58:59 +0100

    wip: translate

---
+++ content/assets/js/translation.js
@@ -0,0 +1,196 @@
+
+var texts = {
+    "en": {
+      "about": "About",
+      "projects": "Projects",
+      "contact": "Contact",
+      "landing-text": "We code so you can chill! WebTree - where code and creativity meet to rock your digital world. Let's take your online game to the next level!",
+      "text-writer": "Individualised for your success!",
+      "about-us-text": "Two young, highly motivated web developers, always on the hunt for the maximum. We are 100% coding action, and we are on a mission to deliver only the best. With full power and passion, we conjure up customised websites for our customers.",
+      "innovative": "Innovative",
+      "reliable": "Reliable",
+      "handmade": "Handmade",
+      "our-services": "Our Services",
+      "offer1": "What our agency ",
+      "offer2": "offers",
+      "development": "Development",
+      "realisation": "Realisation",
+      "development-text": "We help you to create a great website that looks good and works well. From the idea to the finished page, we take care of everything. With our support, your digital business card will be a success on the Internet.",
+      "cost-transparency": "Cost transperency",
+      "safety": "Safety",
+      "functionality": "Functionality",
+      "well-thought-out-code": "Well thought-out code",
+      "maintenance": "Maintenance",
+      "hosting-text": "We help you to ensure that your website is not only optimally developed, but also runs on a hosting service so that your web project shines and you can offer your visitors first-class performance.",
+      "reliable-hosting": "Reliable hosting",
+      "seamless-integration": "Seamless integration",
+      "scalability": "Scalability",
+      "search-optimisation": "Search optimisation",
+      "design": "Organisation",
+      "design-text": "We support you in creating a unique and appealing website. From design to technical implementation, we ensure that your website both looks good and works smoothly.",
+      "responsive-design": "Responsive design",
+      "creative-design": "Creative design",
+      "loading-time": "Fast loading times",
+      "aesthetics": "Aesthetics",
+      "customer-successes": "Customer successes",
+      "a-flash": "A flash of our",
+      "creativity": "Creativity",
+      "burger-text": "RAI expert simplifies care documentation for health insurance companies. We support our customers on their path to independence with our website.",
+      "colin-text": "Colin Indergand shares his trading knowledge on his website. We have developed this platform to expand and share his expert knowledge.",
+      "md-text": "MDCoaching digitises and updates fitness coaching for today's generation and brings reliable fitness offers closer. Our mission is to support you.",
+      "urban-text": "Discover UrbanForge: an architect website mockup by WebTree that showcases innovative designs and inspiring projects.",
+      "to-website": "Visit Website",
+      "contact-us": "Contact us!",
+      "your": "Your",
+      "website": "Website",
+      "deserves-it": "deserves it!",
+      "firstname": "First-name",
+      "lastname": "Last-name",
+      "mail": "Mail",
+      "message": "Message",
+      "send-now": "Send message",
+      "error-message": "Oops, something went wrong. Try again!",
+      "insecurities": "Insecurities",
+      "we-explain": "We explain it to",
+      "to-you": "You",
+      "faq1-question": "You",
+      "faq2-question": "You",
+      "faq3-question": "You",
+      "faq4-question": "You",
+      "faq5-question": "You",
+      "faq6-question": "You",
+      "faq1-answer": "If you submit a successful project, you will receive a bonus of CHF 100!",
+      "faq2-answer": "No, absolutely not. With us you are in good hands and receive professional support so that every website can be realised according to your wishes.",
+      "faq3-answer": "We realise various types of websites, including static websites, multipagers, one-pagers, online shops and many more. There is certainly something for everyone who needs a website.",
+      "faq4-answer": "No, we are a creative team that would like to bring even outdated projects back up to date in order to bring the digital presence to the best level.",
+      "faq5-answer": "The duration of the project realisation depends on the complexity of your request. As a rule, we realise projects promptly and require an average of around one to two months.",
+      "faq6-answer": "The costs vary depending on the functions and scope of your website. Our team will be happy to provide you with a free consultation. Do not hesitate to contact us!",
+      "location": "Zurich, Switzerland",
+      "presentation": "Presentation",
+      "imprint": "Imprint",
+      "data-protection": "Data protection",
+    },
+    "de": {
+        "about": "Über uns",
+        "projects": "Projekte",
+        "contact": "Kontakt",
+        "landing-text": "Wir coden, damit Du chillen kannst! WebTree - wo Code und Kreativität sich treffen, um Deine digitale Welt zu rocken. Lass uns Dein Online-Game auf das nächste Level bringen!",
+        "text-writer": "Individuell für Deinen Erfolg!",
+        "about-us-text": "Zwei junge, hochmotivierte Webentwickler, immer auf der Jagd nach dem Maximum. Bei uns ist 100% Coding-Action angesagt, und wir sind auf einer Mission, nur das Beste zu liefern. Mit voller Power und Leidenschaft zaubern wir massgeschneiderte Webseiten für unsere Kunden.",
+        "innovative": "Innovativ",
+        "reliable": "Zuverlässig",
+        "handmade": "Handgefertigt",
+        "our-services": "Unsere Services",
+        "offer1": "Was unsere Agentur ",
+        "offer2": "bietet",
+        "development": "Entwicklung",
+        "realisation": "Realisierung",
+        "development-text": "Wir helfen Dir, eine tolle Webseite zu erstellen, die gut aussieht und funktioniert. Von der Idee bis zur fertigen Seite kümmern wir uns um alles. Mit unserer Unterstützung wird deine digitale Visitenkarte ein Erfolg im Internet.",
+        "cost-transparency": "Kostentransparenz",
+        "safety": "Sicherheit",
+        "functionality": "Funktionalität",
+        "well-thought-out-code": "Durchdachter Code",
+        "maintenance": "Wartung",
+        "hosting-text": "Wir helfen Dir, sicherzustellen, dass Deine Webseite nicht nur optimal entwickelt ist, sondern auch auf einem Hosting-Service läuft, damit Dein Webprojekt strahlt und Du Deinen Besuchern eine erstklassige Performance bieten kannst.",
+        "reliable-hosting": "Zuverlässiges Hosting ",
+        "seamless-integration": "nahtlose Integration",
+        "scalability": "Skalierbarkeit",
+        "search-optimisation": "Suchoptimierung",
+        "design": "Gestaltung",
+        "design-text": "Wir unterstützen Dich bei der Gestaltung einer einzigartigen und ansprechenden Webseite. Vom Design bis zur technischen Umsetzung sorgen wir dafür, dass deine Webseite sowohl gut aussieht als auch reibungslos funktioniert.",
+        "responsive-design": "Responsives design",
+        "creative-design": "Kreative Gestaltung",
+        "loading-time": "Schnelle Ladezeiten",
+        "aesthetics": "Ästhetik",
+        "customer-successes": "Kundenerfolge",
+        "a-flash": "Hier blitzt unsere",
+        "creativity": "Kreativität",
+        "burger-text": "RAI-Experte vereinfacht die Pflegedokumentation für Krankenkassen. Wir unterstützten unseren Kunden bei seinem Weg in die Selbstständigkeit mit unserer Webseite.",
+        "colin-text": "Colin Indergand teilt sein Trading-Wissen auf seiner Webseite. Wir haben diese Plattform entwickelt, um sein Expertenwissen zu erweitern und zu teilen.",
+        "md-text": "MDCoaching digitalisiert und aktualisiert Fitness-Coaching für die heutige Generation und bringt verlässliche Fitnessangebote näher. Unsere Mission ist es, Dich zu unterstützen.",
+        "urban-text": "Entdecke UrbanForge: Ein Architekten-Webseiten-Mockup von WebTree, das innovative Designs und inspirierende Projekte präsentiert.",
+        "to-website": "Zur Webseite",
+        "contact-us": "Kontaktiere uns!",
+        "your": "Deine",
+        "website": "Webseite",
+        "deserves-it": "verdient's!",
+        "firstname": "Vrname",
+        "lastname": "Nachname",
+        "mail": "E-Mail",
+        "message": "Nâchricht",
+        "send-now": "Nachricht senden",
+        "error-message": "Hoppla! Etwas ist schief gelaufen. Versuche es erneut!",
+        "insecurities": "Unsicherheiten",
+        "we-explain": "Wir erklären es ",
+        "to-you": "Dir",
+        "faq1-question": "You",
+        "faq2-question": "You",
+        "faq3-question": "You",
+        "faq4-question": "You",
+        "faq5-question": "You",
+        "faq6-question": "You",
+        "faq1-answer": "Das Einbringen eines erfolgreichen Projektes führt dazu, dass du einen Bonus von CHF 100 erhältst!",
+        "faq2-answer": "Nein, absolut nicht. Bei uns bist Du gut aufgehoben und erhältst professionelle Unterstützung, damit jede Webseite nach deinem Wunsch realisert werden kann.",
+        "faq3-answer": "Wir realisieren verschiedene Arten von Webseiten, darunter statische Webseiten, Multipager, Onepager, Online-Shops und viele weitere. Da ist sicher etwas für jeden dabei, der eine Webseite braucht.",
+        "faq4-answer": "Nein, wir sind ein kreatives Team, das gerne auch veraltete Projekte wieder auf den neuesten Stand bringen möchte, um den digitalen Auftritt auf das beste Level zu bringen.",
+        "faq5-answer": "Die Dauer der Projektumsetzung hängt von der Komplexität Deines Wunsches ab. In der Regel setzen wir Projekte zeitnah um und benötigen im Durchschnitt etwa einen bis zwei Monate.",
+        "faq6-answer": "Die Kosten variieren je nach den Funktionen und dem Umfang Deiner Webseite. Für eine kostenlose Beratung steht Dir unser Team gerne zur Verfügung. Zögere nicht, uns zu kontaktieren!",
+        "location": "Zürich, Schweiz",
+        "presentation": "Präsentation",
+        "imprint": "Impressum",
+        "data-protection": "Datenschutz",
+    }
+  };
+
+// Funktion zum Aktualisieren der Texte basierend auf der ausgewählten Sprache
+var enButton = document.getElementById('en-link');
+var deButton = document.getElementById('de-link');
+
+function updateTexts(language) {
+    var elements = document.querySelectorAll('[data-lang]');
+    elements.forEach(function(element) {
+        var key = element.getAttribute('data-lang');
+        element.textContent = texts[language][key];
+    });
+
+    // Überprüfen, ob die Sprache tatsächlich geändert wurde
+    var currentLanguage = localStorage.getItem('language');
+    if (currentLanguage !== language) {
+        // Speichern der Spracheinstellung im localStorage
+        localStorage.setItem('language', language);
+    }
+
+    // Entferne zuerst alle Markierungen von Buttons
+
+    // Markiere den entsprechenden Button, falls vorhanden
+    if (language === 'en') {
+        deButton.classList.remove('highlighted');
+        enButton.classList.add('highlighted');
+        document.getElementById("send-btn").value = "Send Message";
+    } else {
+      enButton.classList.remove('highlighted');
+        deButton.classList.add('highlighted');
+        document.getElementById("send-btn").value = "Nachricht senden";
+    }
+}
+
+// Überprüfen und Laden der Spracheinstellung aus dem localStorage beim Laden der Seite
+window.addEventListener('DOMContentLoaded', function() {
+    var savedLanguage = localStorage.getItem('language');
+    if (savedLanguage) {
+        updateTexts(savedLanguage);
+    } else {
+        // Wenn keine Spracheinstellung im localStorage gefunden wurde, standardmäßig auf Deutsch starten
+        updateTexts('de');
+    }
+});
+
+// Beispiel: Wenn der Benutzer auf den Link für Deutsch klickt
+deButton.addEventListener('click', function() {
+    updateTexts('de');
+});
+
+// Beispiel: Wenn der Benutzer auf den Link für Englisch klickt
+enButton.addEventListener('click', function() {
+    updateTexts('en');
+});
